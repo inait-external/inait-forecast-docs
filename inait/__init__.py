8b0be9879d4f558e7f1a9e98cdffbb8ca13f9d10
Pedro Rodrigues <Pedro.Rodrigues@inait.ai>
2025-08-13 11:34:04 +0200

    revert breaking change on notebook 2 (#21)

--- inait/__init__.py
+++ inait/__init__.py
@@ -11,21 +11,22 @@ from .prediction_script import (
     predict,
 )
 from .plot_script import create_plot_payload, plot_image, bytes_from_base64, plot
-from .explanation_script import create_explanation_payload
+from .explanation_script import explain, create_explanation_payload
 
 __version__ = "0.1.0"
 __author__ = "Inait Team"
 
 __all__ = [
-    "make_request",
-    "make_get_request",
-    "create_payload_from_file",
-    "get_dataframe_from_response",
-    "create_plot_payload",
-    "plot_image",
     "bytes_from_base64",
     "create_explanation_payload",
-    "predict",
+    "create_payload_from_file",
+    "create_plot_payload",
+    "explain",
+    "get_dataframe_from_response",
+    "make_get_request",
+    "make_request",
+    "plot_image",
     "plot",
+    "predict",
     "load_credentials",
 ]
